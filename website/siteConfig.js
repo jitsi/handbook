25077ec448ae9dad5e7d4ed8ac5717fb0c28dc5d
Saúl Ibarra Corretgé <s@saghul.net>
2020-05-16 15:36:22 +0200

    Tweak site settings

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -77,13 +77,13 @@ const siteConfig = {
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
+  docsSideNavCollapsible: true,
 
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
 
   // Show documentation's last update time.
-  // enableUpdateTime: true,
+  enableUpdateTime: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...

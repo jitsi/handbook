bc14c334f1013bfdc0a7f194d046f5da9e83b733
Saúl Ibarra Corretgé <s@saghul.net>
2022-03-25 23:08:24 +0100

    Make sections go directly to the first item
    Also remove it from the sidebar, since it essentially becomes the title.

--- sidebars.js
+++ sidebars.js
@@ -1,83 +1,130 @@
 module.exports = {
-  docs: {
-    "Getting Started": ["intro", "architecture", "security", "faq"],
-    Community: [
-      "community/community-intro",
-      "community/community-instances",
-      "community/breakout-rooms",
-      "community/third-party-software",
-    ],
-    "User Guide": [
-      "user-guide/user-guide-start",
-      "user-guide/supported-browsers",
-      "user-guide/user-guide-join-jitsi-meeting",
-      "user-guide/user-guide-start-a-jitsi-meeting",
-      "user-guide/user-guide-share-a-jitsi-meeting",
-      "user-guide/user-guide-jitsi-meet-on-mobile",
-      "user-guide/user-guide-jitsi-meet-for-google-calendar",
-      "user-guide/keyboard-shortcuts",
-      "user-guide/user-guide-basic",
-      "user-guide/user-guide-advanced",
-    ],
-    "Developer Guide": [
-      "dev-guide/dev-guide-start",
-      "dev-guide/dev-guide-contributing",
-      {
-        type: "category",
-        label: "Web",
-        items: [
-          "dev-guide/dev-guide-web",
-          "dev-guide/dev-guide-ljm",
-          "dev-guide/dev-guide-web-integrations",
-          "dev-guide/dev-guide-iframe",
-          "dev-guide/dev-guide-react-sdk",
-          "dev-guide/dev-guide-ljm-api",
-        ],
+  docs: [
+    {
+      type: "category",
+      label: "Getting Started",
+      items: [
+        "intro",
+        "architecture",
+        "security",
+        "faq",
+      ],
+    },
+    {
+      type: "category",
+      label: "Community",
+      link: {
+        type: "doc",
+        id: "community/community-intro",
       },
-      {
-        type: "category",
-        label: "Mobile",
-        items: [
-          "dev-guide/dev-guide-mobile",
-          "dev-guide/dev-guide-mobile-jitsi-meet",
-          "dev-guide/mobile-feature-flags",
-          "dev-guide/dev-guide-android-sdk",
-          "dev-guide/dev-guide-ios-sdk",
-        ],
+      items: [
+        "community/community-instances",
+        "community/breakout-rooms",
+        "community/third-party-software",
+      ],
+    },
+    {
+      type: "category",
+      label: "User Guide",
+      link: {
+        type: "doc",
+        id: "user-guide/user-guide-start",
       },
-    ],
-    "Self-Hosting Guide": [
-      "devops-guide/devops-guide-start",
-      {
-        type: "category",
-        label: "Deployment",
-        items: [
-          "devops-guide/devops-guide-requirements",
-          "devops-guide/devops-guide-quickstart",
-          "devops-guide/devops-guide-opensuse",
-          "devops-guide/devops-guide-docker",
-          "devops-guide/devops-guide-manual",
-        ],
+      items: [
+        "user-guide/supported-browsers",
+        "user-guide/user-guide-join-jitsi-meeting",
+        "user-guide/user-guide-start-a-jitsi-meeting",
+        "user-guide/user-guide-share-a-jitsi-meeting",
+        "user-guide/user-guide-jitsi-meet-on-mobile",
+        "user-guide/user-guide-jitsi-meet-for-google-calendar",
+        "user-guide/keyboard-shortcuts",
+        "user-guide/user-guide-basic",
+        "user-guide/user-guide-advanced",
+      ],
+    },
+    {
+      type: "category",
+      label: "Developer Guide",
+      link: {
+        type: "doc",
+        id: "dev-guide/dev-guide-start",
       },
-      {
-        type: "category",
-        label: "Configuration",
-        items: [
-          "devops-guide/secure-domain",
-          "devops-guide/ldap-authentication",
-          "devops-guide/devops-guide-scalable",
-          "devops-guide/reservation",
-          "devops-guide/turn",
-          "devops-guide/speakerstats",
-          "devops-guide/videosipgw",
-          "devops-guide/cloud-api",
-        ],
+      items: [
+        "dev-guide/dev-guide-contributing",
+        {
+          type: "category",
+          label: "Web",
+          link: {
+            type: "doc",
+            id: "dev-guide/dev-guide-web",
+          },
+          items: [
+            "dev-guide/dev-guide-ljm",
+            "dev-guide/dev-guide-web-integrations",
+            "dev-guide/dev-guide-iframe",
+            "dev-guide/dev-guide-react-sdk",
+            "dev-guide/dev-guide-ljm-api",
+          ],
+        },
+        {
+          type: "category",
+          label: "Mobile",
+          link: {
+            type: "doc",
+            id: "dev-guide/dev-guide-mobile",
+          },
+          items: [
+            "dev-guide/dev-guide-mobile-jitsi-meet",
+            "dev-guide/mobile-feature-flags",
+            "dev-guide/dev-guide-android-sdk",
+            "dev-guide/dev-guide-ios-sdk",
+          ],
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Self-Hosting Guide",
+      link: {
+        type: "doc",
+        id: "devops-guide/devops-guide-start",
       },
-      "devops-guide/devops-guide-videotutorials",
-      "devops-guide/faq",
-    ],
-  },
-  "releases-sidebar": {
-    Releases: ["releases"],
-  },
+      items: [
+        {
+          type: "category",
+          label: "Deployment",
+          items: [
+            "devops-guide/devops-guide-requirements",
+            "devops-guide/devops-guide-quickstart",
+            "devops-guide/devops-guide-opensuse",
+            "devops-guide/devops-guide-docker",
+            "devops-guide/devops-guide-manual",
+          ],
+        },
+        {
+          type: "category",
+          label: "Configuration",
+          items: [
+            "devops-guide/secure-domain",
+            "devops-guide/ldap-authentication",
+            "devops-guide/devops-guide-scalable",
+            "devops-guide/reservation",
+            "devops-guide/turn",
+            "devops-guide/speakerstats",
+            "devops-guide/videosipgw",
+            "devops-guide/cloud-api",
+          ],
+        },
+        "devops-guide/devops-guide-videotutorials",
+        "devops-guide/faq",
+      ],
+    },
+  ],
+  "releases-sidebar": [
+    {
+      type: "doc",
+      label: "Releases",
+      id: "releases",
+    },
+  ],
 };
